2bbafdbc11529ac585994787e44bc6e860386d84
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-04 00:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.18%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        size = len(board)
+        i = 0
+        j = 0
+        column = {k: set() for k in range(9)}
+        box = {k: set() for k in range(9)}
+        while i < size:
+            row = set()
+            while j < size:
+                val = board[i][j]
+                grp = ((i//3)*3)+(j//3)
+                if val == ".":
+                    pass
+                elif val in row:
+                    return False
+                elif val in column[j]:
+                    return False
+                elif val in box[grp]:
+                    return False
+                else:
+                    row.add(val)
+                    column[j].add(val)
+                    box[grp].add(val)
+                j += 1
+            i += 1
+            j = 0
+        return True
