6ac6ca234aacff8fe2f7a0704c42cb5530456814
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-04 18:27:28 +0530

    Time: 11 ms (35.92%) | Memory: 18.1 MB (57.71%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        k = len(s) - 1
+        s = s.lower()
+        while True:
+            while i < len(s) and not s[i].isalnum():
+                i += 1
+            while k > 0 and not s[k].isalnum():
+                k -= 1
+            if k < i:
+                return True
+            elif s[i] == s[k]:
+                i += 1
+                k -= 1
+            else:
+                return False
+        
