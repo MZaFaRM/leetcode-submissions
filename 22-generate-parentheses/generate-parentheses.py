0cfcecce8a67aa78f449666f005b5056fee70846
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-08 00:29:34 +0530

    Time: 2 ms (35.77%) | Memory: 18 MB (69.85%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,22 +1,16 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        self.result = []
-        self.backtrack(n, n, [])
-        return self.result
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        return list(self.backtrack(n, n, []))
 
     def backtrack(self, left, right, current):
         if right == 0:
-            self.result.append("".join(current))
+            yield "".join(current)
             return
         if left > 0:
             current.append("(")
-            self.backtrack(left-1, right, current)
+            yield from self.backtrack(left-1, right, current)
             current.pop()
         if left < right:
             current.append(")")
-            self.backtrack(left, right-1, current)
+            yield from self.backtrack(left, right-1, current)
             current.pop()
