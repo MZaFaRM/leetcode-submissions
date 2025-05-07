e42b7c33aeffdc6da820519528a8389916f8ee76
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-08 00:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (76.85%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -5,14 +5,18 @@ class Solution(object):
         :rtype: List[str]
         """
         self.result = []
-        self.backtrack(n, n, "")
+        self.backtrack(n, n, [])
         return self.result
 
     def backtrack(self, left, right, current):
         if right == 0:
-            self.result.append(current)
+            self.result.append("".join(current))
             return
         if left > 0:
-            self.backtrack(left-1, right, current + "(")
+            current.append("(")
+            self.backtrack(left-1, right, current)
+            current.pop()
         if left < right:
-            self.backtrack(left, right-1, current + ")")
+            current.append(")")
+            self.backtrack(left, right-1, current)
+            current.pop()
