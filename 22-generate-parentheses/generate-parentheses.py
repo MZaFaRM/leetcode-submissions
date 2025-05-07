26b5693468e68b9641296d7edc6a66cf5f30a0bb
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-08 00:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (48.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -10,13 +10,12 @@ class Solution(object):
 
     def backtrack(self, left, right, current):
         if right == 0:
-            return self.result.append("".join(current))
-
+            self.result.append("".join(current))
+            return
         if left > 0:
             current.append("(")
             self.backtrack(left-1, right, current)
             current.pop()
-            
         if left < right:
             current.append(")")
             self.backtrack(left, right-1, current)
