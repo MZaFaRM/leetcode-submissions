efc5b2f18ab83cd9de075a94b3e280b61e039a0a
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-28 22:27:01 +0400

    Time: 4 ms (99.65%) | Memory: 17.7 MB (82.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,16 +4,24 @@ class Solution:
         i = 0
         j = 0
         subset = set()
+        length = 0
         
         for i in range(len(s)):
             if s[i] not in subset:
                 subset.add(s[i])
             else:
+                if length > longest:
+                    longest = length
+
                 while s[i] in subset:
                     subset.remove(s[j])
                     j += 1
-                subset.add(s[i])
 
-            longest = max(len(subset), longest) 
-        return longest
+                subset.add(s[i])
+            length = len(subset)
+
+        if length > longest: 
+            return length
+        else:
+            return longest
         
