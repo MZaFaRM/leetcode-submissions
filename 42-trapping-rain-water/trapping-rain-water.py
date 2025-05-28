b75dafc26c6f5bfcf959d70cf4ee121904f41d2c
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2025-05-28 20:21:03 +0400

    Time: 3 ms (97.82%) | Memory: 19.2 MB (69.56%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -4,21 +4,20 @@ class Solution:
         high = len(height) - 1
         max_low = height[low]
         max_high = height[high]
-        waters = 0
+        water = 0
 
         while low < high:
             if max_low < max_high:
-                water = max_low - height[low]
                 low += 1
                 if height[low] > max_low:
                     max_low = height[low]
+                else:
+                    water += max_low - height[low]
             else:
-                water = max_high - height[high]
                 high -= 1
                 if height[high] > max_high:
                     max_high = height[high]
-
-            if water > 0:
-                waters += water
-        return waters
+                else:
+                    water += max_high - height[high]
+        return water
                 
